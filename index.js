1e59b4751673da87c388aaa1fab6b3a4630eb6c8
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-02-25 14:21:24 +0600

    jwt verification added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
+//middleware
 app.use(cors());
 app.use(express.json());
 
@@ -28,6 +29,128 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("proWallet").collection("users");
+
+
+
+    //jwt related routes
+
+    app.post("/jwt", async (req, res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
+        res.send({token});
+       })
+
+       const verifyToken = (req, res, next) =>{
+        console.log("inside verify token", req.headers);
+        if(!req.headers.authorization){
+          return res.status(401).send({message: 'unauthorized access'});
+        }
+        const token = req.headers.authorization.split(' ')[1];
+      
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+          if(err){
+            return res.status(401).send({message:"unauthorized access"});
+          }
+          req.decoded= decoded;
+          next();
+        })
+       }
+    
+       // use verify admin after verify token
+
+       const verifyAdmin = async (req, res, next) =>{
+        const email = req.decoded.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        const isAdmin = user?.role === "admin";
+        if(!isAdmin){
+          return res.status(403).send({message: 'forbidden access'})
+        }
+         next();
+       }
+
+
+       // use verify agent after verify token
+
+       const verifyAgent = async (req, res, next) =>{
+        const email = req.decoded.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        const isAgent = user?.role === "agent";
+        if(!isAgent){
+          return res.status(403).send({message: 'forbidden access'})
+        }
+         next();
+       }
+    
+
+       //user related routes
+       
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        user.status = user.role === "agent" ? "requested" : "approved";
+        user.balance = user.role === "agent" ? 100000 : 40 ;
+        const query = {
+            $or: [
+                { email: user.email },
+                { phone: user.phone },
+                { nid: user.nid }
+            ]
+        };
+        const existUser = await usersCollection.findOne(query);
+        if(existUser){
+         return res.send({message: "User already exist!", insertedId: null});
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      })
+
+      app.get("/users",  async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+      });
+
+      app.get('/users/admin/:email', verifyToken, async (req, res) =>{
+        const email = req.params.email;
+        if(email !== req.decoded.email){
+          return res.status(403).send({message: "forbidden access"})
+        }
+       const query = {email: email};
+       const user = await usersCollection.findOne(query);
+       let admin = false;
+       if(user){
+        admin = user.role === "admin";
+       }
+       res.send({admin});
+      })
+
+      app.get('/users/agent/:email', verifyToken, async (req, res) =>{
+        const email = req.params.email;
+        if(email !== req.decoded.email){
+          return res.status(403).send({message: "forbidden access"})
+        }
+       const query = {email: email};
+       const user = await usersCollection.findOne(query);
+       let agent = false;
+       if(user){
+        agent = user.role === "agent";
+       }
+       res.send({agent});
+      })
+
+      app.get('/users/email/:email', async (req, res) =>{
+        const email = req.params.email;
+        const query = {email: email};
+        const result = await usersCollection.findOne(query);
+        res.send(result);
+      })
+  
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
